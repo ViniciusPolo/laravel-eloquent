7471eeefeb8e0ae2c46304a09c294b6df134569b
ViniciusPolo <admviniciuspolo@gmail.com>
2022-02-04 14:46:03 -0300

    clientfactory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ClientFactory extends Factory
@@ -14,7 +15,10 @@ class ClientFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'id_number' => Client::factory()->create()->id
         ];
     }
 }
